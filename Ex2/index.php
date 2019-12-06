ba5dc5bcef274ca7dc6a2c9668e9e5f73cfb3406
EmilieLamps <emilie.lamps@gmail.com>
2019-12-06 11:08:49 +0100

    Fin de l'exercice

--- Ex2/index.php
+++ Ex2/index.php
@@ -1,11 +1,18 @@
 <?php
 $page = "Exercice 2"; // Définir la variable pour changer le titre !
-
+$isEasy = true;
 include '../header.php';
  ?>
  <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 2</h2>
- <h3 class="text-center pb-5"></h3>
+ <h3 class="text-center pb-5">Créer une variable isEasy de type booléan et l'initialiser avec une valeur.
+Afficher C'est facile!! si c'est vrai. Dans le cas contraire afficher C'est difficile !!!.
+Bonus : L'écrire de deux manières différentes.</h3>
 <?php
-echo  $message; //Pas de = entre echo et la requête !
+if ($isEasy) {
+  echo 'C\'est facile !!';
+}
+else {
+  echo 'C\'est difficile !!!';
+}
 include '../footer.php';
 ?>
