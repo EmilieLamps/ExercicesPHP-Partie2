770bc2f74f5b6321284f420c1f9a6decf76d9da0
EmilieLamps <emilie.lamps@gmail.com>
2019-12-06 15:53:13 +0100

    Fin de l'exercice

--- Ex6/index.php
+++ Ex6/index.php
@@ -1,12 +1,22 @@
 <?php
-$page = "Exercice 1"; // Définir la variable pour changer le titre !
-$name = 'Emilie';
-$message = 'Bonjour ' .$name . ' comment vas-tu ?'; //faire les espaces avant ou après la chaine de caractères
+$page = "Exercice 6"; // Définir la variable pour changer le titre !
+$age = 18;
+$message = '';
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 1</h2>
- <h3 class="text-center pb-5"></h3>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 6</h2>
+ <h3 class="text-center pb-5">Traduire ce code avec des if et des else :
+  <!-- echo ($age >= 18) ? 'Tu es majeur' : 'Tu n\'es pas majeur'; -->
+</h3>
 <?php
-echo  $message; //Pas de = entre echo et la requête !
+if ($age >= 18) {
+  $message = 'Tu es majeur';
+}
+else {
+  $message = 'Tu n\'es pas majeur';
+}
+?>
+<p><?= $message ?></p>
+<?php
 include '../footer.php';
 ?>
