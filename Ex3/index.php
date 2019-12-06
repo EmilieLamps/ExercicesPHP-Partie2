00b325b6d20ae4e102938062066a3e6dd1632dba
EmilieLamps <emilie.lamps@gmail.com>
2019-12-06 14:04:19 +0100

    Fin de l'exercice

--- Ex3/index.php
+++ Ex3/index.php
@@ -1,12 +1,31 @@
 <?php
-$page = "Exercice 1"; // Définir la variable pour changer le titre !
-$name = 'Emilie';
-$message = 'Bonjour ' .$name . ' comment vas-tu ?'; //faire les espaces avant ou après la chaine de caractères
+$page = "Exercice 3";
+$age = 18;
+$gender = true;
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
- <h3 class="text-center pb-5"></h3>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 3</h2>
+ <h3 class="text-center pb-5">Créer deux variables age et gender. La variable gender peut prendre comme valeur :
+    Homme
+    Femme
+En fonction de l'âge et du genre afficher la phrase correspondante :
+    Vous êtes un homme et vous êtes majeur
+    Vous êtes un homme et vous êtes mineur
+    Vous êtes une femme et vous êtes majeur
+    Vous êtes une femme et vous êtes mineur
+Gérer tous les cas.</h3>
 <?php
-echo  $message; //Pas de = entre echo et la requête !
+if ($age >=18 AND $gender) {
+  echo "Vous êtes une femme et vous êtes majeur";
+}
+elseif ($age <18 AND $gender) {
+  echo "Vous êtes une femme et vous êtes mineur";
+}
+elseif ($age >=18 AND !$gender) {
+  echo "Vous êtes un homme et vous êtes majeur";
+}
+else {
+  echo "Vous êtes un homme et vous êtes mineur";
+}
 include '../footer.php';
 ?>
