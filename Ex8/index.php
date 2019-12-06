9469ff1df164632bb45da33703937d3995bda8e2
EmilieLamps <emilie.lamps@gmail.com>
2019-12-06 16:03:47 +0100

    Fin de l'exercice

--- Ex8/index.php
+++ Ex8/index.php
@@ -1,12 +1,23 @@
 <?php
-$page = "Exercice 1"; // Définir la variable pour changer le titre !
-$name = 'Emilie';
-$message = 'Bonjour ' .$name . ' comment vas-tu ?'; //faire les espaces avant ou après la chaine de caractères
+$page = "Exercice 8"; // Définir la variable pour changer le titre !
+$isOk = true;
+$message = '';
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 1</h2>
- <h3 class="text-center pb-5"></h3>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 8</h2>
+ <h3 class="text-center pb-5">
+Traduire ce code avec des if et des else :
+  <!-- echo ($isOk) ? 'c'est ok !!' : 'c'est pas bon !!!'; -->
+</h3>
 <?php
-echo  $message; //Pas de = entre echo et la requête !
+if (isOk) {
+  $message = 'c\'est ok !!';
+}
+else {
+  $message = 'c\'est pas bon !!!';
+}
+?>
+<p><?= $message ?></p>
+<?php
 include '../footer.php';
 ?>
