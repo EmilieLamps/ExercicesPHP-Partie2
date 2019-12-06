f0c597c8054604c5508b3d6ca83ae6aaf29eef7a
EmilieLamps <emilie.lamps@gmail.com>
2019-12-06 15:47:27 +0100

    Fin de l'exercice

--- Ex5/index.php
+++ Ex5/index.php
@@ -1,12 +1,22 @@
 <?php
-$page = "Exercice 1"; // Définir la variable pour changer le titre !
-$name = 'Emilie';
-$message = 'Bonjour ' .$name . ' comment vas-tu ?'; //faire les espaces avant ou après la chaine de caractères
+$page = "Exercice 5"; // Définir la variable pour changer le titre !
+$gender = false;
+$message = ' ';
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
- <h3 class="text-center pb-5"></h3>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 5</h2>
+ <h3 class="text-center pb-5">Traduire ce code avec des if et des else :
+<!-- echo ($gender != 'Homme') ? 'C\'est une développeuse !!!' : 'C\'est un développeur !!!';-->
+</h3>
 <?php
-echo  $message; //Pas de = entre echo et la requête !
+if (!$gender) {
+  $message = 'C\'est une développeuse !!!';
+}
+else {
+  $message = 'C\'est un développeur !!!';
+}
+?>
+<p><?= $message ?></p>
+<?php
 include '../footer.php';
 ?>
